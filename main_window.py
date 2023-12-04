7a893cf0ee141e48bd90fb15a24ffe4488114e6c
gregory-237 <petrovgrigorij29@gmail.com>
2023-12-04 22:36:56 +0400

    remake all program to best view(part 2)

--- main_window.py
+++ main_window.py
@@ -1,148 +1,127 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+# import csv
+# from functools import partial
+from PyQt5 import QtWidgets, QtGui, QtCore
+from script1 import split_by_columns
+from script2 import split_by_years
+from script3 import split_by_weeks
+from script4 import find_in_dataset
 
-class Ui_MainWindow(object):
-    def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(997, 491)
-        self.centralwidget = QtWidgets.QWidget(MainWindow)
-        self.centralwidget.setStyleSheet("background-color: white;")
-        self.centralwidget.setObjectName("centralwidget")
-        self.frame = QtWidgets.QFrame(self.centralwidget)
-        self.frame.setGeometry(QtCore.QRect(0, 0, 1001, 101))
-        self.frame.setStyleSheet("background-color: #a0a0a0;")
-        self.frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
-        self.frame.setFrameShadow(QtWidgets.QFrame.Raised)
-        self.frame.setObjectName("frame")
-        self.label = QtWidgets.QLabel(self.frame)
-        self.label.setGeometry(QtCore.QRect(430, 20, 171, 51))
-        font = QtGui.QFont()
-        font.setPointSize(14)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label.setFont(font)
-        self.label.setObjectName("label")
-        self.label_2 = QtWidgets.QLabel(self.centralwidget)
-        self.label_2.setGeometry(QtCore.QRect(0, 110, 491, 251))
-        self.label_2.setText("")
-        self.label_2.setPixmap(QtGui.QPixmap("lab3/figure.png"))
-        self.label_2.setScaledContents(True)
-        self.label_2.setObjectName("label_2")
-        self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(590, 110, 321, 41))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_3.setFont(font)
-        self.label_3.setStyleSheet("text-align: center;")
-        self.label_3.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_3.setObjectName("label_3")
-        self.input_line = QtWidgets.QLineEdit(self.centralwidget)
-        self.input_line.setGeometry(QtCore.QRect(510, 190, 171, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        self.input_line.setFont(font)
-        self.input_line.setStyleSheet("border: 2px solid #666666;\n"
-"border-radius: 100;\n"
-"")
-        self.input_line.setText("")
-        self.input_line.setAlignment(QtCore.Qt.AlignCenter)
-        self.input_line.setObjectName("input_line")
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(700, 190, 93, 41))
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        font.setBold(True)
-        font.setWeight(75)
-        self.pushButton.setFont(font)
-        self.pushButton.setStyleSheet("QPushButton{\n"
-"border: 2px solid #666666;\n"
-"border-radius: 100;\n"
-"}\n"
-"\n"
-"QPushButton:pressed{\n"
-"background-color: #a0a0a0;\n"
-"}")
-        self.pushButton.setObjectName("pushButton")
-        self.output_line = QtWidgets.QLineEdit(self.centralwidget)
-        self.output_line.setGeometry(QtCore.QRect(812, 190, 171, 41))
-        font = QtGui.QFont()
-        font.setPointSize(12)
-        font.setStyleStrategy(QtGui.QFont.PreferDefault)
-        self.output_line.setFont(font)
-        self.output_line.setStyleSheet("border: 2px solid #666666;\n"
-"border-radius: 100;\n"
-"")
-        self.output_line.setText("")
-        self.output_line.setAlignment(QtCore.Qt.AlignCenter)
-        self.output_line.setObjectName("output_line")
-        self.label_4 = QtWidgets.QLabel(self.centralwidget)
-        self.label_4.setGeometry(QtCore.QRect(530, 160, 131, 21))
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_4.setFont(font)
-        self.label_4.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_4.setObjectName("label_4")
-        self.label_5 = QtWidgets.QLabel(self.centralwidget)
-        self.label_5.setGeometry(QtCore.QRect(844, 160, 111, 21))
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        font.setBold(True)
-        font.setWeight(75)
-        self.label_5.setFont(font)
-        self.label_5.setAlignment(QtCore.Qt.AlignCenter)
-        self.label_5.setObjectName("label_5")
-        self.pushButtonPath = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButtonPath.setGeometry(QtCore.QRect(520, 260, 221, 51))
-        font = QtGui.QFont()
-        font.setPointSize(10)
-        font.setBold(True)
-        font.setWeight(75)
-        self.pushButtonPath.setFont(font)
-        self.pushButtonPath.setStyleSheet("QPushButton{\n"
-"border: 2px solid #666666;\n"
-"border-radius: 100;\n"
-"}\n"
-"\n"
-"QPushButton:pressed{\n"
-"background-color: #a0a0a0;\n"
-"}")
-        self.pushButtonPath.setObjectName("pushButtonPath")
-        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
-        self.comboBox.setGeometry(QtCore.QRect(762, 261, 221, 51))
-        font = QtGui.QFont()
-        font.setPointSize(9)
-        font.setBold(True)
-        font.setWeight(75)
-        self.comboBox.setFont(font)
-        self.comboBox.setLayoutDirection(QtCore.Qt.LeftToRight)
-        self.comboBox.setAutoFillBackground(False)
-        self.comboBox.setStyleSheet("QComboBox{\n"
-"border: 2px solid #666666;\n"
-"border-radius: 100;\n"
-"}")
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(MainWindow)
-        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Dollar Rates")
+        self.setWindowIcon(QtGui.QIcon('img/web.png'))
+        self.folderpath = None
+        self.dataset = None
+        self.menu = None
+        self.destination_folder = None
 
-    def retranslateUi(self, MainWindow):
-        _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label.setText(_translate("MainWindow", "Курс Доллара"))
-        self.label_3.setText(_translate("MainWindow", "Узнать курс за определенный день"))
-        self.pushButton.setText(_translate("MainWindow", "ТЫК"))
-        self.label_4.setText(_translate("MainWindow", "ДАТА"))
-        self.label_5.setText(_translate("MainWindow", "ЗНАЧЕНИЕ"))
-        self.pushButtonPath.setText(_translate("MainWindow", "Выберите папку"))
-        self.comboBox.setItemText(0, _translate("MainWindow", "dataset"))
-        self.comboBox.setItemText(1, _translate("MainWindow", "X и Y"))
-        self.comboBox.setItemText(2, _translate("MainWindow", "разбивка по годам"))
-        self.comboBox.setItemText(3, _translate("MainWindow", "разбивка по неделям"))
+        '''Widgets'''
+        self.label = QtWidgets.QLabel("Выберите папку с исходным датасетом:")
+        self.select_folder_button = QtWidgets.QPushButton("Выбрать папку")
+        self.select_folder_button.clicked.connect(self.select_folder)
+
+        self.create_dataset_button = QtWidgets.QPushButton("Создать датасет")
+        self.create_dataset_button.clicked.connect(self.create_dataset)
+
+        self.date_input = QtWidgets.QDateEdit()
+        self.date_input.setCalendarPopup(True)
+        self.date_input.setDate(QtCore.QDate.currentDate())
+
+        self.get_data_button = QtWidgets.QPushButton("Получить данные")
+        self.get_data_button.clicked.connect(self.get_date)
+
+        '''Layout'''
+        layout = QtWidgets.QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.select_folder_button)
+        layout.addWidget(self.create_dataset_button)
+        layout.addWidget(self.date_input)
+        layout.addWidget(self.get_data_button)
+
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(layout)
+        self.setCentralWidget(central_widget)
+
+    def select_folder(self):
+        self.folderpath = QtWidgets.QFileDialog.getExistingDirectory(self, 'Выберите папку')
+        if self.folderpath:
+            QtWidgets.QMessageBox.information(self, 'Папка выбрана', f'Выбрана папка: {self.folderpath}')
+        self.folderpath += "/dataset.csv"
+
+    def create_dataset(self):
+        if not self.folderpath:
+            QtWidgets.QMessageBox.warning(self, 'Ошибка', 'Выберите папку с исходным датасетом')
+            return
+
+        self.destination_folder = QtWidgets.QFileDialog.getExistingDirectory(self, 'Выберите папку для нового датасета')
+        if self.destination_folder:
+            self.menu = SubWindow(self)
+            self.menu.show()
+
+    def get_date(self):
+        if not self.folderpath:
+            QtWidgets.QMessageBox.warning(self, 'Ошибка', 'Выберите папку с исходным датасетом')
+            return
+
+        selected_date = self.date_input.date().toString(QtCore.Qt.ISODate)
+        self.find_date(selected_date)
+
+    def find_date(self, selected_date):
+        course = find_in_dataset(selected_date, self.folderpath)
+        if course == None:
+            QtWidgets.QMessageBox.information(self, 'Данные не получены', f'Курса доллара на {selected_date} нет')
+        else:
+            QtWidgets.QMessageBox.information(self, 'Данные получены',
+                                              f'Курс доллара на {selected_date} равен {course} руб')
+
+
+class SubWindow(QtWidgets.QDialog):
+    def __init__(self, Main: MainWindow = None):
+        super(SubWindow, self).__init__(Main)
+        self.setModal(True)
+
+        if Main is not None:
+            self.Main = Main
+
+        layout = QtWidgets.QVBoxLayout()
+
+        self.button1 = QtWidgets.QPushButton("Разделить на X и Y")
+        self.button1.clicked.connect(self.but1)
+        self.button2 = QtWidgets.QPushButton("Разделить по годам")
+        self.button2.clicked.connect(self.but2)
+        self.button3 = QtWidgets.QPushButton("Разделить по неделям")
+        self.button3.clicked.connect(self.but3)
+
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.button3)
+
+        self.setLayout(layout)
+
+    def but1(self):
+        split_by_columns(self.Main.folderpath, self.Main.destination_folder)
+        QtWidgets.QMessageBox.information(self, 'Датасет создан',
+                                          f'Датасет создан и сохранен в {self.Main.destination_folder}')
+        self.close()
+
+    def but2(self):
+        split_by_years(self.Main.folderpath, self.Main.destination_folder)
+        QtWidgets.QMessageBox.information(self, 'Датасет создан',
+                                          f'Датасет создан и сохранен в {self.Main.destination_folder}')
+        self.close()
+
+    def but3(self):
+        split_by_weeks(self.Main.folderpath, self.Main.destination_folder)
+        QtWidgets.QMessageBox.information(self, 'Датасет создан',
+                                          f'Датасет создан и сохранен в {self.Main.destination_folder}')
+        self.close()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
